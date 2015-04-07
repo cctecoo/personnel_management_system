1148a2e47fc6fc7b9200847eb1fda2647e4802c1
cctecoo <358553880@qq.com>
2015-04-07 14:12:56 +0800

    first commit

---
+++ personnel_management_system/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'personnel_management_system.views.home', name='home'),
+    # url(r'^personnel_management_system/', include('personnel_management_system.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'personnel_management_system.views.first_page'),
+)
---
+++ personnel_management_system/views.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponse
+
+
+def first_page(request):
+    return HttpResponse("<p>hello</p>")
