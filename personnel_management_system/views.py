2c478c3ef88cec3435fb2eedff471e663789129c
cctecoo <358553880@qq.com>
2015-04-29 16:57:01 +0800

    完成index 公告轮播。fix some bug

--- personnel_management_system/views.py
+++ personnel_management_system/views.py
@@ -3,8 +3,12 @@
 import datetime
 
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, render
+
+from notice.models import Notice
+from personnel_management_system import settings
 
 
 @login_required
@@ -12,5 +16,22 @@ def index_view(request):
     """
     首页View
     """
+    today = datetime.datetime.now().strftime(settings.DATETIME_INPUT_FORMATS[3])
 
-    return render(request, 'index/index.html',)
+    # 取出符合时间段的公告，按照创建时间倒序排（最新）
+    queryset = Notice.objects.filter(start_date__lt=today, end_date__gt=today).\
+        exclude(delete_flg=True).order_by("-create_datetime")
+
+    # 非系统管理员 只能查看自己所属部门的公告
+    if not request.user.is_superuser:
+        str_department_id = str(request.user.personal.department.id)
+        plus_department_id = str_department_id + ','
+        queryset = queryset.filter(
+            Q(dep_id__contains=plus_department_id) |
+            Q(dep_id=str_department_id)
+            )
+
+    return render(request, "index/index.html", {
+        "notices": queryset,
+
+    })
